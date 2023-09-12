12c5871503ebab7b31d907d683623d6ce278743a
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-13 05:37:53 +0600

    Update Thinking Lighter.py

--- Some Random Problems/Thinking Lighter.py
+++ Some Random Problems/Thinking Lighter.py
@@ -1,20 +1,20 @@
 n = 5
 pos1, pos2 = [4, 3]
 obs = [[2, 3], [4, 2], [5, 5]]
-
-down = [[pos1-i,pos2] for i in range(1,pos1)]
-print(down)
-
-up = [[pos1+i,pos2] for i in range(1,n-pos1+1)]
-print(up)
-
-left = [[pos1, pos2-i] for i in range(1,n-pos2+1)]
-print(left)
-right = [[pos1, pos2+i] for i in range(1,n-pos2+1)]
-print(right)
-
-right_up = [[pos1+i, pos2+i] for i in range(1,n-max(pos1,pos2)+1)]
-print(right_up)
-left_up = [[pos1+i, pos2-i] for i in range(1,n-max(pos1,pos2)+1)]
-print(left_up)
-
+moves={}
+moves["down"] = [[pos1-i,pos2] for i in range(1,pos1)]
+moves["up"]= [[pos1+i,pos2] for i in range(1,n-pos1+1)]
+moves["left"] = [[pos1, pos2-i] for i in range(1,n-pos2+1)]
+moves["right"] = [[pos1, pos2+i] for i in range(1,n-pos2+1)]
+moves["right_up"] = [[pos1+i, pos2+i] for i in range(1,n-max(pos1,pos2)+1)]
+moves["left_up"]= [[pos1+i, pos2-i] for i in range(1,n-max(pos1,pos2)+1)]
+#right down here
+#left down here
+print(moves)
+total=0
+for move in moves.values():
+    for m in move:
+        if m in obs:
+            break
+        total+=1
+print(total)
