483e87c55949bd549d80dcb8048340b74cd8fe51
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-13 03:46:04 +0600

    Calculate total Moves

--- Some Random Problems/Queens Attack v3.py
+++ Some Random Problems/Queens Attack v3.py
@@ -71,7 +71,6 @@ def left_up(pos1,pos2,obs,n):
     pos=[pos1,pos2]
     while pos[0]<n and pos[1]>1:
         pos[0],pos[1]=[pos1,pos2][0]+1,pos[1]-1
-        print(pos1,pos2)
         if pos in obs:
             break
         else:
@@ -89,9 +88,17 @@ def left_down(pos1,pos2,obs):
             left_down+=1
     return left_down
 
+n = 5
+pos1, pos2 = [4, 3]
+obs = [[2, 3], [4, 2], [5, 5]]
 
-n=5
-pos1,pos2=[4,3]
-obs = [[2,3],[4,2],[5,5]]
-print(left(pos1,pos2,obs))
-print(right(pos1,pos2,obs,n))
+down_result = down(pos1, pos2, obs)
+up_result = up(pos1, pos2, obs, n)
+left_result = left(pos1, pos2, obs)
+right_result = right(pos1, pos2, obs, n)
+right_up_result = right_up(pos1, pos2, obs, n)
+right_down_result = right_down(pos1, pos2, obs, n)
+left_up_result = left_up(pos1, pos2, obs, n)
+left_down_result = left_down(pos1, pos2, obs)
+total_moves = down_result + up_result + left_result + right_result + right_up_result + right_down_result + left_up_result + left_down_result
+print("Total moves:", total_moves)
