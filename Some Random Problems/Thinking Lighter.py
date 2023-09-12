ae670e0e9bbc68bcb2de3ca4098d47b8129ab83f
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-13 05:56:10 +0600

    Update Thinking Lighter.py

--- Some Random Problems/Thinking Lighter.py
+++ Some Random Problems/Thinking Lighter.py
@@ -4,12 +4,12 @@ obs = [[2, 3], [4, 2], [5, 5]]
 moves={}
 moves["down"] = [[pos1-i,pos2] for i in range(1,pos1)]
 moves["up"]= [[pos1+i,pos2] for i in range(1,n-pos1+1)]
-moves["left"] = [[pos1, pos2-i] for i in range(1,n-pos2+1)]
+moves["left"] = [[pos1, pos2-i] for i in range(1,pos2)]
 moves["right"] = [[pos1, pos2+i] for i in range(1,n-pos2+1)]
 moves["right_up"] = [[pos1+i, pos2+i] for i in range(1,n-max(pos1,pos2)+1)]
 moves["left_up"]= [[pos1+i, pos2-i] for i in range(1,n-max(pos1,pos2)+1)]
-#right down here
-#left down here
+moves["right_down"] = [[pos1-i, pos2+i] for i in range(1,n-min(pos1,pos2)+1)]
+moves["left_down"] = [[pos1-i, pos2-i] for i in range(1,n-min(pos1,pos2)+1)]
 print(moves)
 total=0
 for move in moves.values():
