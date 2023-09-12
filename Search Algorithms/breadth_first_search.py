5d473e7a6bbd2b31ebc0b9350ef27bf5d6e496ee
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-12 23:10:21 +0600

    BFS

--- Search Algorithms/breadth_first_search.py
+++ Search Algorithms/breadth_first_search.py
@@ -1,18 +1,8 @@
 from collections import deque
-graph = {}
-
-graph["you"] = ["alice", "bob", "claire"]
-graph["bob"] = ["anuj","peggy"]
-graph["alice"] = ["peggy"]
-graph["claire"] = ["thom","johnny M"]
-graph["anuj"] = []
-graph["peggy"] = []
-graph["thom"]=[]
-graph["johnny"] = []
-print(graph)
 
 def person_is_seller(person):
     return person[-1] == "M"
+    
 def bfs(graph, name):
     search_queue = deque()
     search_queue += graph[name]
@@ -30,7 +20,18 @@ def bfs(graph, name):
             else:
                 print(f"{person} is already searched!")
     return False
-print(bfs(graph,"you"))
+    
+if __name__ == "__main__":
+    graph = {}
+    graph["you"] = ["alice", "bob", "claire"]
+    graph["bob"] = ["anuj","peggy"]
+    graph["alice"] = ["peggy"]
+    graph["claire"] = ["thom","johnny M"]
+    graph["anuj"] = []
+    graph["peggy"] = []
+    graph["thom"]=[]
+    graph["johnny"] = []
+    print(bfs(graph,"you"))
 
 
 
