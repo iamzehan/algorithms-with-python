9f4e43b3be09353b433860dcc6137ab22d24c586
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-12 07:32:33 +0600

    Update create_the_graphs.py

--- Dijkstra's Algorithm/create_the_graphs.py
+++ Dijkstra's Algorithm/create_the_graphs.py
@@ -1,5 +1,5 @@
 graph = {} 
-nodes=input("Name the nodes:").replace(""," ").replace(","," ").split()
+nodes=input("Name the nodes:\n").replace(""," ").replace(","," ").split()
 start=input("Define your start Node:\n")
 end=nodes.pop(nodes.index(input("Define your finish Node:\n")))
 for node in nodes:
@@ -9,5 +9,7 @@ for node in nodes:
 	graph[node][neighbor]= int(input(f"Cost {node} -> {neighbor}:\n"))
 graph[end]=None
 print(graph)
-costs = {node: graph[start][node] if node in graph[start] else float("inf") for node in graph.keys() if node != start}
+costs = {node: graph[start][node] if node in graph[start] else float("inf") 
+	 for node in graph.keys() 
+	 if node != start}
 parents = {node: start for node in graph[start].keys()}
