65e3299d6ae379a5b219da8ee367b681bcf98c09
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-12 06:30:00 +0600

    Update create_the_graph.py

--- Dijkstra's Algorithm/create_the_graph.py
+++ Dijkstra's Algorithm/create_the_graph.py
@@ -1,11 +1,11 @@
 graph = {} 
 nodes=list(input("What are the name of the Nodes?\n"))
-
-for i in nodes[:-1]:
+end=nodes.pop(nodes.index(input("Define you finsh Node:\n")))
+for i in nodes:
     graph[i]={} 
     neighbors=list(input(f"Neighbors of {i}:\n"))
     for j in neighbors :
         graph[i][j]= int(input(f"Cost {i} -> {j}:\n"))
-graph[nodes[-1]]=None
+graph[end]=None
 print(graph)
        
