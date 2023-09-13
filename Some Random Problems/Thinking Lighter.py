1c1d4de079cbe41af95f1927d003744b55f49b9b
Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2023-09-13 08:01:11 +0600

    Update Thinking Lighter.py

--- Some Random Problems/Thinking Lighter.py
+++ Some Random Problems/Thinking Lighter.py
@@ -1,15 +1,15 @@
 n = 5
 pos1, pos2 = [4, 3]
-obs = [[2, 3], [4, 2], [5, 5]]
+obs = [[2,3], [4,2],[5,5]]
 moves={}
 moves["down"] = [[pos1-i,pos2] for i in range(1,pos1)]
 moves["up"]= [[pos1+i,pos2] for i in range(1,n-pos1+1)]
 moves["left"] = [[pos1, pos2-i] for i in range(1,pos2)]
 moves["right"] = [[pos1, pos2+i] for i in range(1,n-pos2+1)]
 moves["right_up"] = [[pos1+i, pos2+i] for i in range(1,n-max(pos1,pos2)+1)]
-moves["left_up"]= [[pos1+i, pos2-i] for i in range(1,n-max(pos1,pos2)+1)]
-moves["right_down"] = [[pos1-i, pos2+i] for i in range(1,n-min(pos1,pos2)+1)]
-moves["left_down"] = [[pos1-i, pos2-i] for i in range(1,n-min(pos1,pos2)+1)]
+moves["left_up"]= [[pos1+i, pos2-i] for i in range(1,min(pos1,pos2)) if pos1+i<=n]
+moves["right_down"] = [[pos1-i, pos2+i] for i in range(1,pos1) if pos2+i<=n]
+moves["left_down"] = [[pos1-i, pos2-i] for i in range(1,min(pos1,pos2))]
 print(moves)
 total=0
 for move in moves.values():
