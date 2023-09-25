c7b3f6c5f55ffb1563b39dd064e89a68fd4aebda
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-09-25 21:46:20 +0600

    Create Tree_sum.py

---
+++ Search Algorithms/Tree_sum.py
@@ -0,0 +1,36 @@
+"""
+Write a function, tree_sum, that takes in the root of a binary tree and calculates the sum of each node.
+"""
+
+class Node:
+    def __init__(self, root):
+        self.root = root
+        self.right = None
+        self.left = None
+def tree_sum(node):
+    if node == None: return 0
+    return node.root + tree_sum(node.right) + tree_sum(node.left)
+    
+    
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+print(tree_sum(a)) # >> 25
+
+
