43632a00fb54180de6edfb94faf952c6bcb74fb1
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-02 02:30:43 +0600

    Time: O(n²) | Space: O(1)

--- Sorting Algorithms/inline_selection_sort.py
+++ Sorting Algorithms/inline_selection_sort.py
@@ -1,12 +1,9 @@
-# We have practiced selection sort, now let's make the space complexity a bit better, O(1)
 def inline_selection_sort(array):
     for i in range(0,len(array)):
         j = array.index(min(array[i:]))
-        minimum = array[j]
-        current = array[i]
         if j > i:
-            array[i] = minimum
-            array[j] = current
+            array[i],array[j]=array[j],array[i]
         elif j ==i:
             continue
     return array
+print(inline_selection_sort([2,7,3,5,1,4]))
