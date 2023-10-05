1f3c4255ee811e3047ee493d38d7a39410b19048
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-05 23:51:39 +0600

    Partitioning Substrings based on being Palindrome

--- Backtracking/PalindromePartitioning.py
+++ Backtracking/PalindromePartitioning.py
@@ -3,7 +3,8 @@ def dfs(i,res,part):
     if i>=len(s):
         res.append(part[:])
     for j in range(i,len(s)):
-        if s[i:j+1] == s[i:j+1][::-1]:
+        k = s[i:j+1]
+        if k == k[::-1]:
             part.append(s[i:j+1])
             dfs(j+1,res,part)
             part.pop()
