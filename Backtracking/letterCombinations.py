20e13d9bc3afaeff9db74fc0a9955bc027103f3d
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-06 23:30:36 +0600

    Letter Combinations of a Phone Number

---
+++ Backtracking/letterCombinations.py
@@ -0,0 +1,27 @@
+def letterCombinations(self, digits):
+    """
+    :type digits: str
+    :rtype: List[str]
+    """
+    digitsToChar = { 
+        "2" : "abc",
+        "3" : "def",
+        "4" : "ghi",
+        "5" : "jkl",
+        "6" : "mno",
+        "7" : "pqrs",
+        "8" : "tuv",
+        "9" : "wxyz"
+        }
+    def backtrack(i, res, currStr):
+        if len(currStr)==len(digits):
+            res.append(currStr)
+            return 
+        for c in digitsToChar[digits[i]]:
+            backtrack(i+1, res, currStr+c)
+        return res
+    
+    if digits:
+        return backtrack(0, [], "")
+    else:
+        return []
