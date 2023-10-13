513c8579191d2e9c31d5aabdbe794d4008f5707b
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-13 23:39:56 +0600

    Letter Combinations of a Phone number

--- Backtracking/letterCombinations.py
+++ Backtracking/letterCombinations.py
@@ -25,3 +25,78 @@ def letterCombinations(digits):
         return backtrack(0, [], "")
     else:
         return []
+
+
+"""
+backtrack("")
+	#doesn't hit base case
+	for c in "abc":
+		c= "a"
+		backtrack("a")
+			# doesn't hit base case
+			for c in "def":
+				c = "d"
+				backtrack("ad")
+					#hits base case
+					["ad"]  #res
+					return 
+				c="e"
+				backtrack("ae")
+					#hits base case
+					["ad","ae"] #res
+					return
+				c="f"
+				backtrack("af")
+					#hits base case
+					["ad","ae","af"] #res
+					return
+				#for loop ends
+			return  ["ad","ae","af"] #res
+		#------------------------ "a" done --------------------------
+		c = "b"
+		backtrack("b")
+			#doesn't hit base case
+			#for c in "def"
+				c="d"
+				backtrack("bd")
+					#hits base case
+					["ad","ae","af","bd"] #res
+					return
+				c="e"
+				backtrack("be")
+					#hits base case
+					["ad","ae","af","bd","be"] #res
+					return 
+				c="f"
+				backtrack("bf")
+					#hits base case
+					["ad","ae","af","bd","be","bf"] #res
+					return
+				#for loop ends
+			return  ["ad","ae","af","bd","be","bf"] #res
+		#------------------------ "b" done --------------------------
+		c = "c"
+		backtrack("c")
+			#doesn't hit base case
+			for c in "def"
+				c="d"
+				backtrack("cd")
+					#hits base case
+					["ad","ae","af","bd","be","bf","cd"] #res
+					return 
+				c="e"
+				backtrack("ce")
+					#hits base case
+					["ad","ae","af","bd","be","bf","cd","ce"] #res
+					return
+				c="f"
+				backtrack("cf")
+					#hits base case
+					["ad","ae","af","bd","be","bf","cd","ce","cf"]  #res
+					return
+				#for loop ends
+			return ["ad","ae","af","bd","be","bf","cd","ce","cf"]  #res
+		#------------------------ "c" done --------------------------
+		
+	return ["ad","ae","af","bd","be","bf","cd","ce","cf"] #res
+"""
