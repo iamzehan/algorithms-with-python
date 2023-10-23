dd321486139a8f5846a5d9b85d2f3518869ae756
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-23 16:07:27 +0600

    Find the Longest Common Subsequence between two strings

---
+++ Dynamic Programming/Longest Common Subsequence.py
@@ -0,0 +1,15 @@
+def LongestCommonSubSequence(text1:str, text2:str) -> int:
+    dp = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
+    for r in range(len(text1)-1, -1, -1):
+        for c in range(len(text2)-1, -1, -1):
+            if text1[r] == text2[c]:
+                dp[r][c] = dp[r+1][c+1]+1
+            else:
+                dp[r][c] = max(dp[r][c+1],dp[r+1][c])
+    return dp[0][0]
+
+if __name__ == '__main__':
+  text1 = "abcde"
+  text2 = "ace"
+  result = LongestCommonSubSequence(text1, text2)
+  print(result)
