9ad166946a49b03516ba142fc3015eebd95d1bcc
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-11-02 20:26:37 +0600

    MaximumSubarray

---
+++ Greedy Algorithm/MaxSubarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums):
+        maxSub = nums[0]
+        curSum = 0
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum+=n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+
+s = Solution()
+array = [-2,1,-3,4,-1,2,1,-5,4]
+result = s.maxSubArray(array)
+print(result)
