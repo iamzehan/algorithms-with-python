af98a6fe83d6420f0233de2f6fe1f00b70b7ab3a
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-11-19 16:37:51 +0600

    hackerrank solution

--- Some Random Problems/maximize_it.py
+++ Some Random Problems/maximize_it.py
@@ -1,21 +1,21 @@
 import heapq
-def product(lists, i, li, prod, res, maxHeap=[]):
-    if len(prod)==len(lists):
-        modulo = -1* (sum(x**2 for x in prod)%1000)
+def maximize_it(arrays, M, i = 0, li =0, prod =[], maxHeap=[]):
+    if len(prod)==len(arrays):
+        modulo = -1* (sum(x**2 for x in prod)%M)
         heapq.heappush(maxHeap, modulo)
         return
-    for j in range(i,len(lists[li])):
-        prod.append(lists[li][j])
-        product(lists,i, li+1, prod, res)
+    for j in range(i,len(arrays[li])):
+        prod.append(arrays[li][j])
+        maximize_it(arrays, M, i, li+1, prod, maxHeap)
         prod.pop()
     return maxHeap[0]*(-1)
     
 if __name__== '__main__':
-    lists=[
+    arrays=[
         [2, 5, 4],
         [3, 7, 8, 9],
         [5, 5, 7, 8, 9, 10]
         ]
-    maxModulo=0
-    result = product(lists,i=0, li=0, prod=[], res=[])
+    M = 1000
+    result = maximize_it(arrays, M)
     print(result)
