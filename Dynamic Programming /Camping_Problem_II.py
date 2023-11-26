d292b38eb24fa03af31eb2eaa84502e79591ce2a
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-11-27 00:47:40 +0600

    Update Camping_Problem_II

--- Dynamic Programming /Camping_Problem_II.py
+++ Dynamic Programming /Camping_Problem_II.py
@@ -5,7 +5,9 @@ class DynamicProgramming:
             for col in range(1, total_capacity+1):
                 if items[row-1]['weight']<=col:
                     candidate1 = dp[row-1][col]
-                    candidate2 = (dp[row-1][col-items[row-1]['weight']][0]+items[row-1]['value'], (dp[row-1][col-items[row-1]['weight']][1]+", "+items[row-1]['name']).strip(',')) # (total_value, items_taken)
+                    candidate2 = (dp[row-1][col-items[row-1]['weight']][0]+items[row-1]['value'], 
+                                 (dp[row-1][col-items[row-1]['weight']][1]+", "
+                                  +items[row-1]['name']).strip(', ')) # (total_value, items_taken)
                     dp[row][col]=max(candidate1,candidate2)
                 else:
                     dp[row][col] = dp[row-1][col]
@@ -23,6 +25,7 @@ if __name__ == '__main__':
     number_of_items = len(items)
     dp = DynamicProgramming()
     result = dp.camping_with_knapsack(items, total_capacity, number_of_items) 
+    print(result)
     total_value, items = result[number_of_items][total_capacity]
     print("Optimal set of items to take: ",items)
     print(f"Total value: ${total_value}")
